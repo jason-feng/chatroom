1ed3a918db818974b20f06a7922bf26b07d7ac5f
Jason Feng <jason21feng@gmail.com>
2015-09-20 18:50:35 +0100

    Add message model

--- client/models.py
+++ client/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(User, null=True)
+    # username field is useful to store guest name of unauthenticated users
+    username = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    content = models.CharField(max_length=5000)
