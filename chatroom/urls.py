1d1c3630d8e9901a1fc43fca94a3cdc0650ac418
Jason Feng <jason21feng@gmail.com>
2015-09-21 11:16:05 +0100

    Add urls for log in and registration

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 
 urlpatterns = [
+    url(r'^$', auth_views.login, {'redirect_field_name': '/chat/'}),
+    url(r'^chat/', include('client.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url('^register/', CreateView.as_view(template_name='registration/register.html', form_class=UserCreationForm, success_url='/')),
 ]
