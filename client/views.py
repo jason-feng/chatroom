1e6fcb420c0cd3c41afd8a91ec020b6e15cf1973
Jason Feng <jason21feng@gmail.com>
2015-09-21 11:15:40 +0100

    Add login restrictions to chatroom

--- client/views.py
+++ client/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Message
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def chatroom(request):
+    messages = Message.objects.order_by('date')
+    context = {'messages': messages}
+    return render(request, 'client/index.html', context)
